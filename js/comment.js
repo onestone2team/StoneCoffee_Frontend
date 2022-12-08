dde488353b3639a65b39367f003e3966eab8860f
hyojine <touroz@naver.com>
2022-12-09 02:32:23 +0900

    feat: 댓글 및 대댓글 조회창 틀 완성

--- js/comment.js
+++ js/comment.js
@@ -1,9 +1,11 @@
 console.log('안녕')
-const loremIpsum = document.getElementById("lorem-ipsum")
+// 없애도될듯
+const detail_page = document.getElementById("detail_page")
 
 fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
     .then(response => response.text())
-    .then(result => loremIpsum.innerHTML = result)
+    .then(result => detail_page.innerHTML = result)
+
 
 const modal = document.getElementById("modal")
 
@@ -36,9 +38,36 @@ modal.addEventListener("click", e => {
         modalOff()
     }
 })
-
+//esc눌렀을때 꺼짐
 window.addEventListener("keyup", e => {
     if(isModalOn() && e.key === "Escape") {
         modalOff()
     }
 })
+
+//게시글 불러오기
+window.onload=async function ProductDetail() {
+
+    const payload = localStorage.getItem("payload")
+    const parsed_payload = JSON.parse(payload)
+    console.log(parsed_payload)
+
+    // if(!parsed_payload){
+    //     alert("권한이 없습니다. 로그인 해주세요")
+    //     location.replace("../templates/main.html")
+    // }
+
+    // $("#headers").load("../templates/navigation.html");
+
+    const product_detail = await fetch(`${BACKEND_URL}/product/${product_id}/view/`, {
+        headers: {
+            'content-type': 'application/json',
+            "Authorization": "Bearer " + localStorage.getItem("access")
+        },
+        method: 'GET',
+    })
+
+    product_detail_json = await product_detail.json()
+    console.log(product_detail_json)
+    comments = product_detail_json.comments_set
+}//Product detail
