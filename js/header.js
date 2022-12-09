49a62cc4c88162bf88717f63a67255a79406ee80
명현 <z9x80123@gmail.com>
2022-12-09 10:57:17 +0900

    feat: my order list, inquiry_list

--- js/header.js
+++ js/header.js
@@ -4,62 +4,60 @@ fetch(` `)
     })
     .then(data => {
         document.querySelector("header").innerHTML =
-            `
-        <div class="aa-header-top">
-        <div class="container">
-            <div class="row">
-            <div class="col-md-12">
-                <div class="aa-header-top-area">
-                <!-- start header top left -->
-                <div class="aa-header-top-left">
-                </div>
-                <!-- / header top left -->
-                <div class="aa-header-top-right">
-                    <ul class="aa-head-top-nav-right">
-                    <li><a href="account.html">내 계정</a></li>
-                    <li class="hidden-xs"><a href="wishlist.html">좋아요</a></li>
-                    <li class="hidden-xs"><a href="cart.html">장바구니</a></li>
-                    <li class="hidden-xs"><a href="checkout.html">결제하기</a></li>
-                    <li class="hidden-xs"><a href="servicecenter.html">고객 센터</a></li>
-                    <li><a href="" data-toggle="modal" data-target="#login-modal">로그인</a></li>
-                    </ul>
+        `<div class="aa-header-top">
+            <div class="container">
+                <div class="header_top_row">
+                    <div class="col-md-12">
+                        <div class="aa-header-top-area">
+                        <!-- start header top left -->
+                            <div class="aa-header-top-left">
+                            </div>
+                            <!-- / header top left -->
+                            <div class="aa-header-top-right">
+                                <ul class="aa-head-top-nav-right">
+                                <li><a href="account.html">내 계정</a></li>
+                                <li class="hidden-xs"><a href="wishlist.html">좋아요</a></li>
+                                <li class="hidden-xs"><a href="cart.html">장바구니</a></li>
+                                <li class="hidden-xs"><a href="checkout.html">결제하기</a></li>
+                                <li class="hidden-xs"><a href="servicecenter.html">고객 센터</a></li>
+                                <li><a href="" data-toggle="modal" data-target="#login-modal">로그인</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
         <!-- / header top  -->
 
         <!-- start header bottom  -->
         <div class="aa-header-bottom">
-        <div class="container">
-            <div class="row">
-            <div class="col-md-12">
-                <div class="aa-header-bottom-area">
-                <!-- logo  -->
-                <div class="aa-logo" padding="30px">
-                    <a href="main.html">
-                    <p>STONE<strong>COFFEE</strong> <span>Your Shopping Partner</span></p>
-                    <!-- <img src="../img/logo.png"  height="250px" width="250px"  >--!>
-                    </a>
-                </div>
-                <!-- / cart box -->
-                <!-- search box -->
-                <div class="aa-search-box">
-                    <form action="">
-                    <input type="text" name="" id="" placeholder="검색할 상품명을 입력해주세요.">
-                    <button type="submit"><span class="fa fa-search">검색 </span></button>
-                    </form>
-                </div>
+            <div class="container">
+                <div class="hearder_bottom_row">
+                    <div class="col-md-12">
+                        <div class="aa-header-bottom-area">
+                            <!-- logo  -->
+                            <div class="aa-logo" padding="30px">
+                                <a href="main.html">
+                                <p>STONE<strong>COFFEE</strong> <span>Your Shopping Partner</span></p>
+                                <!-- <img src="../img/logo.png"  height="200px" width="200px"  >--!>
+                                </a>
+                            </div>
+                            <!-- / cart box -->
+                            <!-- search box -->
+                            <div class="aa-search-box">
+                                <form action="">
+                                <input type="text" name="" id="" placeholder="검색할 상품명을 입력해주세요.">
+                                <button type="submit"><span class="fa fa-search">검색 </span></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
-        </div>
-
-`;
+        </div>`;
     });
+
 fetch(`../index.html`)
     .then(response => {
         return response.text()
