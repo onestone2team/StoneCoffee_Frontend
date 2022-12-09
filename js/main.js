1247394809ecd90f31ad1fd8a32bfa561d79b02c
Beam-ki <skunn123@gmail.com>
2022-12-09 15:51:59 +0900

    feat : js/ import 변경

--- js/main.js
+++ js/main.js
@@ -20,17 +20,18 @@ async function show_product_list() {
         var products = document.getElementById("products");
         // $("#products").empty();
         for (i = 0; i < 8; i++) {
-            console.log(data["data"]["goods"])
+            console.log(data["data"]["coffee"])
+            console.log(data["data"])
             const product = document.createElement('p')
             // product.setAttribute("class", "aa-product-catg","style","max-width: 150; height: 150;")
             product.innerHTML = `<li>
             <figure>
-                  <a  id="img" class="aa-product-img" href="product-detail.html?product_id=${data["data"]["coffee"][i]["id"]}/"><img style="max-width: 100%; height: 100%;" src="${BACK_END_URL}${data["data"]["coffee"][i]["image"]}" alt="${data["data"]["coffee"][i]["id"]}"></a>
+                  <a  id="img" class="aa-product-img" href="product-detail.html?product_id=${data["data"][i]["id"]}/"><img style="max-width: 100%; height: 100%;" src="${BACK_END_URL}${data["data"][i]["image"]}" alt="${data["data"][i]["id"]}"></a>
               <a class="aa-add-to-cart-btn" onclick="cart()">장바구니 담기</a>
-              <a class="aa-add-to-cart-btn" onclick="like()">좋아요</a>
+              <a class="aa-add-to-cart-btn" onclick="like()">좋s아요</a>
                 <figcaption>
-                    <h4 class="aa-product-title">${data["data"]["coffee"][i]["product_name"]}</h4>
-                    <span class="aa-product-price">${data["data"]["coffee"][i]["price"]}원</span><span class="aa-product-price"></span>
+                    <h4 class="aa-product-title">${data["data"][i]["product_name"]}</h4>
+                    <span class="aa-product-price">${data["data"][i]["price"]}원</span><span class="aa-product-price"></span>
                 </figcaption>
             </figure>                        
           </li>`
