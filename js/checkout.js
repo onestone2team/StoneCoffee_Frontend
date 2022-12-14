5b2eb101123c751cdd46a39c5f324205671d86a5
seungjuyu <2seungju0@gmail.com>
2022-12-15 04:07:21 +0900

    수정

--- js/checkout.js
+++ js/checkout.js
@@ -2,7 +2,7 @@ console.log("연결")
 
 cartlist = []
 
-// =====장바구니 리스트 불러오기=========
+// =====장바구니 리스트 불러오기========= //
 
 window.onload=async function checkoutlist(){
     const response = await fetch (`${BACK_END_URL}/product/cart`, {
@@ -16,6 +16,7 @@ window.onload=async function checkoutlist(){
     const order_frame = document.getElementById('append-product')
         response_json.forEach(element => {
             const order = document.createElement('div')
+            var total_price = element.price * element.count
             console.log(element.id)
             cartlist.push(element.id)
             console.log(cartlist)
@@ -27,7 +28,7 @@ window.onload=async function checkoutlist(){
                             <td><img src="${BACK_END_URL}${element.product["image"]}" class="tdimg"></td>
                             <td class="product" id="product">${element.product.product_name}</td>
                             <td class="count" id="count" >${element.count}</td>
-                            <td class="price" id="price">${element.price}원</td>  
+                            <td class="price" id="price">${total_price}원</td>  
                             </tr>
                             </table>
                         </input>
@@ -37,8 +38,34 @@ window.onload=async function checkoutlist(){
         )
 } 
 
+//===주문금액====//
+total = 0
+async function recalculateCart() {
 
-// =====개인정보 작성======
+    let num = document.getElementById("subtotal").innerHTML.slice(0, -1) * 1;
+    total = total + num
+    $("#basket-subtotal").html(total)
+    if (total >= 50000) {
+        $('#delivery_price').fadeOut(fadeTime) && $('#delivery_price2').fadeIn(fadeTime);
+    } else {
+        $('#delivery_price2').fadeOut(fadeTime) && $('#delivery_price').fadeIn(fadeTime);
+    }
+    delivery_price = $("#delivery_price").innerHTML * 1;
+    final_total = total + delivery_price
+    $('.final-value').fadeIn(fadeTime, function () {
+        $('#basket-total').html(total);
+        if (total == 0) {
+            $('.checkout-cta').fadeOut(fadeTime);
+        } else {
+            $('.checkout-cta').fadeIn(fadeTime);
+        }
+        $('.final-value').fadeIn(fadeTime);
+    });
+};
+
+
+
+// =====개인정보 작성====== //
 async function fillin(){
     let A=document.getElementById("cart_id")["placeholder"];    
     console.log(A)
@@ -63,7 +90,13 @@ async function fillin(){
 
     
         }),
-
-
 })
-} 
+    
+if (kind in [0,1,2] & title != "" & content !="" ){
+   if (confirm("상품주문을 하시겠습니까?") == true){
+   } else {
+   }
+}
+
+}
+
