98f5717bbecf5f6e2225465235bff1867c81b72e
JoonPyoJang <jjpyo1009@gmail.com>
2022-12-18 21:00:14 +0900

    update: admin 일때 이름 클릭시 이동 주소 변경

--- js/header.js
+++ js/header.js
@@ -5,12 +5,18 @@ access_token = localStorage.getItem("access")
 const loginText = document.getElementById("loginout")
 
 if(access_token) {
+    console.log(parsed_payload["profilename"])
     const username = document.getElementById("user")
+    console.log(username)
     username.innerText = `${parsed_payload["profilename"]}님`
     loginText.innerText = "로그아웃"
+    if(parsed_payload["is_admin"]==true){
+        username.href = "admin_order_list.html"
+    }
 } else {
     loginText.innerText = "로그인"
 }
+
 
 async function logoutUser() {
     kakao_token = localStorage.getItem("kakao")
