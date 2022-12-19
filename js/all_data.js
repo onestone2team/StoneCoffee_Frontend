69114a0af5d876607f042f782da91841dcbe97b7
hyojine <touroz@naver.com>
2022-12-19 11:28:29 +0900

    chore: change backend url

--- js/all_data.js
+++ js/all_data.js
@@ -1,2 +1,3 @@
 const FRONT_END_URL = "http://localhost:5500"
-const BACK_END_URL = "https://www.musinsa.shop"
+// const BACK_END_URL = "https://www.musinsa.shop"
+const BACK_END_URL = "http://127.0.0.1:8000"
