c74b2737aedbec11e9becdc66e3b19e2172cef28
JoonPyoJang <jjpyo1009@gmail.com>
2022-12-22 15:47:52 +0900

    update: header 문의하기 및 프로필및 유저 이름 추가 #104

--- js/header.js
+++ js/header.js
@@ -6,7 +6,7 @@ var loginText = document.getElementById("loginout")
 
 if(access_token) {
     const username = document.getElementById("user")
-    username.innerText = `${parsed_payload["profilename"]}님`
+    username.innerText = `${parsed_payload["profilename"]}님 반갑습니다`
     loginText.innerText = "로그아웃"
     if(parsed_payload["is_admin"]==true){
         username.href = "admin_order_list.html"
