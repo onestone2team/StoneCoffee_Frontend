079045c828ef036ae594a3e9ef6a8830d23cb949
Beam-ki <skunn123@gmail.com>
2022-12-23 11:39:08 +0900

    fix 검색창 enter키 사용가능

--- js/rolling.js
+++ js/rolling.js
@@ -1,47 +1,47 @@
-var banner_left = 0;
-var img_cnt = 0;
-var first=1;
-var last;
-var interval;
+// var banner_left = 0;
+// var img_cnt = 0;
+// var first=1;
+// var last;
+// var interval;
 
-window.onload = function(){
-    rolling();
-}
-function rolling() {
-	$(".rolling_wrap a").each(function() {
-		$(this).css("left", banner_left);
-		banner_left += $(this).width()+5;
-		$(this).attr("id", "content"+(++img_cnt));
-	});
+// window.onload = function(){
+//     rolling();
+// }
+// function rolling() {
+// 	$(".rolling_wrap a").each(function() {
+// 		$(this).css("left", banner_left);
+// 		banner_left += $(this).width()+5;
+// 		$(this).attr("id", "content"+(++img_cnt));
+// 	});
 
-	last = img_cnt;
-	startAction();
+// 	last = img_cnt;
+// 	startAction();
 
-	$(".content").hover(
-		function() { stopAction(); }, 
-		function() { startAction(); });
-};
+// 	$(".content").hover(
+// 		function() { stopAction(); }, 
+// 		function() { startAction(); });
+// };
 
-function startAction() {
-	interval = setInterval(function() {
-		$(".rolling_wrap a").each(function() {
-			$(this).css("left", $(this).position().left-2);
-		});
+// function startAction() {
+// 	interval = setInterval(function() {
+// 		$(".rolling_wrap a").each(function() {
+// 			$(this).css("left", $(this).position().left-2);
+// 		});
 
-		var first_content = $("#content"+first);
-		var last_content = $("#content"+last);
+// 		var first_content = $("#content"+first);
+// 		var last_content = $("#content"+last);
 
-		if(first_content.position().left < "-"+$(first_content).width()) {
-			first_content.css("left", last_content.position().left+last_content.width()+5);
-			first++;
-			last++;
-			if(last > img_cnt) { last = 1;}
-			if(first > img_cnt) {first = 1;}
-		}
-	}, 15);
-}
+// 		if(first_content.position().left < "-"+$(first_content).width()) {
+// 			first_content.css("left", last_content.position().left+last_content.width()+5);
+// 			first++;
+// 			last++;
+// 			if(last > img_cnt) { last = 1;}
+// 			if(first > img_cnt) {first = 1;}
+// 		}
+// 	}, 15);
+// }
 
-function stopAction() {
-	clearInterval(interval);
-}
+// function stopAction() {
+// 	clearInterval(interval);
+// }
 
