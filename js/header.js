359c8f5e7d551fd4fd176a166d1f16921c5f2704
Beam-ki <skunn123@gmail.com>
2022-12-26 06:21:50 +0900

    수정

--- js/header.js
+++ js/header.js
@@ -55,9 +55,9 @@ async function timeOut() {
     // console.log(payload["exp"])
     // console.log(payload["iat"])
     // console.log(payload["user_id"])
-    // console.log(Date.now)
+    // console.log(Date.now()/1000)
 
-    if (payload.exp <(Date.now()/7200)){
+    if (payload.exp <(Date.now()/1000)){
         localStorage.removeItem("access")
         localStorage.removeItem("refresh")
         localStorage.removeItem("payload")
@@ -66,30 +66,8 @@ async function timeOut() {
 
         alert("세션이 만료 되어서 로그아웃 되었습니다\n메인페이지로 이동하겠습니다.")
         window.location.replace(`../index.html`);
+    }else{
     };
 }timeOut()
 
 
-
-// 토큰 완료 자동 로그아웃//
-// async function timeOut() {
-//     const payload = JSON.parse(localStorage.getItem("payload"));
-//     if (payload.exp <(Date.now()/1000)){
-//         localStorage.removeItem("access")
-//         localStorage.removeItem("refresh")
-//         localStorage.removeItem("payload")
-//         localStorage.removeItem("user")
-//         localStorage.removeItem("pk")
-//         Swal.fire({
-//             title: '토큰세션이 만료되었습니다!',
-//             text: '다시로그인 해주세요',
-//             icon: 'warning',
-//             confirmButtonColor: '#FFCCCC',
-//             confirmButtonText: '확인',
-//         }).then(result =>{
-//             if(result.isConfirmed){
-//                 window.location.href = "../templates/main.html"
-//             }
-//         })
-//     };
-// }timeOut()
