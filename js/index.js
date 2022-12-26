b544860ee6ac36eb703602c33b282c84d178ab0d
seungjuyu <2seungju0@gmail.com>
2022-12-26 17:21:37 +0900

    update 로그인 후 설문조사가 있을때 설문조사 기반 추천 띄우기 #135

--- js/index.js
+++ js/index.js
@@ -71,3 +71,51 @@ async function checkout() {
     })
 }
 
+// 추천슬라이드//
+
+let slideWrap = $(".slide_wrap"),
+    slideShow = slideWrap.find(".slide_show"),
+    slideList = slideShow.find(".slide_list"),
+    slides = slideList.find(".slide"),
+    slideBtn = slideWrap.find(".slide_btn");
+
+let slideCount = slides.length,
+    slideWidth = slides.innerWidth(),
+    showNum = 3,
+    num = 0,
+    currentIndex = 0,
+    
+    slideCopy = $(".slide:lt("+ showNum +")").clone();
+    slideList.append(slideCopy);
+
+//이미지 움직이기
+function backShow(){
+  if( num == 0 ){
+    //시작
+    num= slideCount;
+    slideList.css("left", -num * slideWidth + "px");
+  }
+  num--;
+  slideList.stop().animate({ left : -slideWidth * num +"px"}, 400);
+}
+
+function nextShow(){
+  if( num == slideCount ){
+    //마지막
+    num= 0;
+    slideList.css("left", num);
+  }
+  num++;
+  slideList.stop().animate({ left : -slideWidth * num +"px"}, 400);
+}
+
+//왼쪽, 오른쪽 버튼 설정
+slideBtn.on("click","button",function(){
+  if( $(this).hasClass("prev")){
+    //왼쪽 버튼을 클릭
+    backShow();
+  } else {
+    //오른쪽 버튼을 클릭
+    nextShow();
+  }
+});
